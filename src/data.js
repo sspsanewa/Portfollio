dc9ba32816acbc7421512893f116d6d8bf6c2dc6
sandeep patidar <spsanewa@gmail.com>
2023-10-20 14:15:04 -0700

    3rd commit

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
       subtitle: "MERN Stack",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "https://www.interviewbit.com/blog/wp-content/uploads/2021/12/React-Projects-800x450.png",
+      image: "https://user-images.githubusercontent.com/79099734/156505537-8e28ee14-dd20-4299-9eea-984d7068c7fd.png",
       link: "https://reactbootcamp.com",
     },
     {
@@ -14,22 +14,6 @@ export const projects = [
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
       image: "https://i.ytimg.com/vi/HfJjzmSaLuQ/maxresdefault.jpg",
       link: "https://reedbarger.com",
-    },
-    {
-      title: "DevChat",
-      subtitle: "React and Firebase",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "https://i.ytimg.com/vi/ly3m6mv5qvg/maxresdefault.jpg",
-      link: "https://jsbootcamp.com",
-    },
-    {
-      title: "Epic Todo App",
-      subtitle: "React Hooks",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20220310072422/7-Best-React-Project-Ideas-for-Beginners-in-2022.png",
-      link: "https://pythonbootcamp.com",
     },
   ];
 
