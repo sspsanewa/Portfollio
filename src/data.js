0111f440da2695f1c93981ca79985f086e51564c
sandeep patidar <spsanewagit@gmail.com>
2024-07-30 17:44:27 +0530

    git push from terminal again build

--- src/data.js
+++ src/data.js
@@ -1,42 +1,43 @@
 import netflix from '../src/Images/netflix.png'
 import figma from '../src/Images/figma.jpg'
-import school from '../src/Images/school.jpg'
+import crypto from '../src/Images/crypto.jpg'
 export const projects = [
-    {
-      title: "Entertainment",
-      subtitle: "(Netflix clone)",
-      description:
-        "Entertainment is a website , which is a clone of Netflix. In the making of this website, the technologies I used are HTML, CSS, JavaScript, React, Firebase for authentication, TMDB API for fetching movies, Redux toolkit for state management, and other modules also.",
-      image: netflix,
-      link: "https://candid-alpaca-ba4f82.netlify.app",
-    },
-    {
-      title: "Figma",
-      subtitle: "Figma pages for ecomerce website",
-      description:
-        "In this project i have created different pages of a ecomerce website like- Admin page, User page, Profile page, Billing page, Order page and Product page.To create this project i have used HTML, CSS, JavaScript, React, Material UI.",
-      image: figma,
-      link: "https://vermillion-conkies-c3b466.netlify.app",
-    },
-    {
-      title: "React Tracks",
-      subtitle: "React and Python",
-      description:
-        "Currently i am working on this project. I am making this website for my village school. To create this project i have used HTML, CSS, JavaScript, React, Material UI.It is responsive also.",
-      image: school,
-      link: "https://leafy-sunflower-b59374.netlify.app",
-    },
-  ];
+  {
+    title: "Crypto Tracker",
+    subtitle: "Track crypto live",
+    description:
+      "Crypto Tracker is a website where you can track cryptocurrency prices live at any time. In the making of this website, I used CSS, JavaScript, React JS, Material UI, Chart JS, the useContext hook for state management, and other modules as well. This website is also hosted on Netlify",
+    image: crypto,
+    link: "https://trackcryptolive.netlify.app/",
+  },
+  {
+    title: "Entertainment",
+    subtitle: "(Netflix clone)",
+    description:
+      "Entertainment is a website , which is a clone of Netflix. In the making of this website, the technologies I used are HTML, CSS, JavaScript, React, Firebase for authentication, TMDB API for fetching movies, Redux toolkit for state management, and other modules also.",
+    image: netflix,
+    link: "https://candid-alpaca-ba4f82.netlify.app",
+  },
+  {
+    title: "Figma",
+    subtitle: "Figma pages for ecomerce website",
+    description:
+      "In this project i have created different pages of a ecomerce website like- Admin page, User page, Profile page, Billing page, Order page and Product page.To create this project i have used HTML, CSS, JavaScript, React, Material UI.",
+    image: figma,
+    link: "https://vermillion-conkies-c3b466.netlify.app",
+  },
 
-  export const skills = [
-    {sk:"C"},
-    {sk:"C++"},
-    {sk:"Html"},
-    {sk:"Css"},
-    {sk:"JavaScript"},
-    {sk:"React.js"},
-    {sk:"Node.js"},
-    {sk:"Express"},
-    {sk:"Mongodb"},
-    {sk:"Git & Github"},
-  ];
+];
+
+export const skills = [
+  { sk: "C" },
+  { sk: "C++" },
+  { sk: "Html" },
+  { sk: "Css" },
+  { sk: "JavaScript" },
+  { sk: "React.js" },
+  { sk: "Node.js" },
+  { sk: "Express" },
+  { sk: "Mongodb" },
+  { sk: "Git & Github" },
+];
